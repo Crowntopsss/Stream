24eb25ee9fe003025ed196b06cf2f72dc8740772
Temitope Adebiyi <adebiyitope26@gmail.com>
2021-04-02 11:21:14 +0100

    verification working

--- Stream Mobile App/Controllers/VerificationViewController.swift
+++ Stream Mobile App/Controllers/VerificationViewController.swift
@@ -9,23 +9,50 @@ import UIKit
 import OTPFieldView
 
 class VerificationViewController: UIViewController {
-
-    @IBOutlet weak var OtpField: OTPFieldView!
+    
+    
+    
+    @IBOutlet weak var otpTextFieldView: OTPFieldView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupOtpView()
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupOtpView(){
+        self.otpTextFieldView.fieldsCount = 6
+        self.otpTextFieldView.fieldBorderWidth = 0.5
+        self.otpTextFieldView.defaultBorderColor = UIColor.black
+        self.otpTextFieldView.filledBorderColor = UIColor.green
+        self.otpTextFieldView.cursorColor = #colorLiteral(red: 0, green: 0.3607843137, blue: 0.9333333333, alpha: 1)
+        self.otpTextFieldView.displayType = .underlinedBottom
+        self.otpTextFieldView.fieldSize = 40
+        self.otpTextFieldView.separatorSpace = 8
+        self.otpTextFieldView.shouldAllowIntermediateEditing = false
+        self.otpTextFieldView.delegate = self
+        self.otpTextFieldView.initializeUI()
     }
-    */
-
+    
+    enum DisplayType: Int {
+        case circular
+        case roundedCorner
+        case square
+        case diamond
+        case underlinedBottom
+    }
+    
+}
+extension VerificationViewController: OTPFieldViewDelegate {
+    func hasEnteredAllOTP(hasEnteredAll hasEntered: Bool) -> Bool {
+        print("Has entered all OTP? \(hasEntered)")
+        return false
+    }
+    
+    func shouldBecomeFirstResponderForOTP(otpTextFieldIndex index: Int) -> Bool {
+        return true
+    }
+    
+    func enteredOTP(otp otpString: String) {
+        print("OTPString: \(otpString)")
+    }
 }
