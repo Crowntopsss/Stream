23ffeda5a72fde3f5ce6d79bec49491afde1f9bc
Temitope Adebiyi <adebiyitope26@gmail.com>
2021-04-02 10:25:22 +0100

    OTP verification

---
+++ Stream Mobile App/Controllers/RegistrationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegistrationViewController.swift
+//  Stream Mobile App
+//
+//  Created by Temitope on 02/04/2021.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func SendVerification(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToVerification", sender:self )
+    }
+    
+}
---
+++ Stream Mobile App/Controllers/VerificationViewController.swift
@@ -0,0 +1,31 @@
+//
+//  VerificationViewController.swift
+//  Stream Mobile App
+//
+//  Created by Temitope on 02/04/2021.
+//
+
+import UIKit
+import OTPFieldView
+
+class VerificationViewController: UIViewController {
+
+    @IBOutlet weak var OtpField: OTPFieldView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
